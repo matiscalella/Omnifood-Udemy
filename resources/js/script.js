f4d31378aa2a2f0b6b419224a0aa7d4d7b857739
Matias Scalella <matias.scalella@live.com>
2018-06-03 00:37:10 -0300

    agrego animaciones con animations.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -64,5 +64,34 @@ $(document).ready(function() {
         }
       });
     
+/*-------------------- Animations on scroll --------------------*/
+    
+    /* Waypoint 1 */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    /* Waypoint 2 */
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    /* Waypoint 3 */
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    /* Waypoint 4 */
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
     
 });
